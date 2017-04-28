009193744d33212cb84bb5c7794907bd1710f674
c0deX1 <sizv1996@gmail.com>
2017-04-28 20:13:28 +0600

    PDO update

--- php/connect.php
+++ php/connect.php
@@ -11,10 +11,8 @@
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
-        PDO::ATTR_PERSISTENT => true,
     ];
     $pdo = new PDO($dsn, $user, $pass, $opt);
-
     return $pdo;
 }
 ?>
