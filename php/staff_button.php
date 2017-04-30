0e8c89c502539c05b35b87750e58a9e56f11dc96
c0deX1 <sizv1996@gmail.com>
2017-04-30 14:35:45 +0600

    hotfix redirect

--- php/staff_button.php
+++ php/staff_button.php
@@ -6,6 +6,7 @@ $trac = $_POST["Tractor"];
 $trail = $_POST["Trailer"];
 
 $pdo = connect();
+$redirect_url = "/path_builder.html";
 
 $stmt = $pdo->prepare("DELETE FROM Crew WHERE Crew.Serial_tractor = ? OR Crew.Serial_trailer = ? OR Crew.ID_first_driver = (Select Cadre.ID_Cadre from Cadre Where Cadre.Surname = ? AND Cadre.Name = ? AND Cadre.Patronymic = ?) OR Crew.ID_second_driver = (Select Cadre.ID_Cadre from Cadre Where Cadre.Surname = ? AND Cadre.Name = ? AND Cadre.Patronymic = ?)");
 $stmt->execute(array(explode(" ", $trac)[2], explode(" ", $trail)[1],explode(" ", $d1)[0],explode(" ", $d1)[1],explode(" ", $d1)[2], explode(" ", $d2)[0], explode(" ", $d2)[1],explode(" ", $d2)[2]));
@@ -17,5 +18,5 @@ if($count)
 	$stmt = $pdo->prepare("INSERT INTO Crew VALUES (?,?,(Select Cadre.ID_Cadre from Cadre Where Cadre.Surname = ? AND Cadre.Name = ? AND Cadre.Patronymic = ?),(Select Cadre.ID_Cadre from Cadre Where Cadre.Surname = ? AND Cadre.Name = ? AND Cadre.Patronymic = ?))");
 	$stmt->execute(array(explode(" ", $trac)[2], explode(" ", $trail)[1],explode(" ", $d1)[0],explode(" ", $d1)[1],explode(" ", $d1)[2], explode(" ", $d2)[0], explode(" ", $d2)[1],explode(" ", $d2)[2]));
 }
-header('Location: http://cis.com/path_builder.html?Driver_1='.$d1.'&Driver_2='.$d2.'&Tractor='.explode(" ", $trac)[2].'&Trailer='.explode(" ", $trail)[1]);
+header('Location: http://'.$_SERVER['HTTP_HOST'].$redirect_url.'?Driver_1='.$d1.'&Driver_2='.$d2.'&Tractor='.explode(" ", $trac)[2].'&Trailer='.explode(" ", $trail)[1]);
 ?>
